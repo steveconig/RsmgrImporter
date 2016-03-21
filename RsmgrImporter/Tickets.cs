078660bc9c3d758c653c88a31d865cc83cf13692
Steve Conigliaro <steveconig@gmail.com>
2016-03-21 17:18:33 -0600

    Starting class commit

---
+++ RsmgrImporter/Tickets.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RsmgrImporter
+{
+    class Tickets
+    {
+        public DataTable TicketTable = new DataTable();
+
+        public void SetTicketTable(DataTable dt)
+        {
+            TicketTable = dt;
+
+        }
+
+
+
+    }
+}
